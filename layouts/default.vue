6f57afa501d4bedf4c150db817bde14d06fb7c1d
bite <bite@Forsites-iMac.local>
2024-11-05 16:34:23 +1300

    checkpoint

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<!-- layouts/default.vue -->
+<template>
+  <div class="layout">
+    <header class="header">
+      <h1>Bite Game Studio</h1>
+      <nav>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/anthology">anthology</NuxtLink>
+      </nav>
+    </header>
+
+    <main class="content">
+      <NuxtPage />
+    </main>
+
+    <footer class="footer">
+      <p>&copy; 2024 Bite Game Studio. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+
+
+<h1>Bite Game Studio</h1>
+<p>The once, future and current homepage of Bite Game Studio.</p>
+</template>
+<script setup>
+
+</script>
